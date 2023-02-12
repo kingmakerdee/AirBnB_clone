346a06326ccd4e35ef35f68ab7b45f077d73dcab
kingmakerdee <denzelndlovu63@gmail.com>
2023-02-12 11:58:47 -0800

    beautiful code that passes pycodestyle checks

---
+++ beauty_code.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# A beautiful code that passes pycodestyle checks
+name = "Denzel"
+age = 30
+str = "I will be an Alx graduate by the end of this year!"
+print(f"My name is {name}. I'm {age} years old.")
+print(f"I'm currently enjoying learning software engineering.")
+print(f"{str}")
