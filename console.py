c7543d24979602dde4ac2b22d0c98d595f38bf3d
kingmakerdee <denzelndlovu63@gmail.com>
2023-02-12 18:23:04 -0800

    update console commands, create, show, destroy, etc...

--- console.py
+++ console.py
@@ -2,6 +2,10 @@
 """ Module for the entry point of the command interpreter"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+import re
+import json
 
 
 class HBNBCommand(cmd.Cmd):
