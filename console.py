1ec17669c8ede436754c5bbabbc4fcde00abe9ec
kingmakerdee <denzelndlovu63@gmail.com>
2023-02-12 16:31:27 -0800

    contains entry point of the command intepreter

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command intepreter which inherits from base class cmd"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """CRTL+D command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Called when empty line + ENTER key,
+        returns newline and does not execute any commands"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
